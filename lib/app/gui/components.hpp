8a3c6483cc438eb050ecb181253c7acf11146fa4
Hossein Naderi <hossein-naderi@hotmail.com>
2026-01-16 12:26:56 +0330

    Removed display

--- lib/app/gui/components.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "lvgl.h"
-
-lv_obj_t *create_text(lv_obj_t *parent, const char *icon, const char *txt,
-                      bool new_track = false);
-
-lv_obj_t *create_text_fmt(lv_obj_t *parent, const char *icon, const char *fmt,
-                          ...);
