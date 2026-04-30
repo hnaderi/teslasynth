89df730aaf51e4c91009fce05f9d0937ab1d15c5
Hossein Naderi <hossein-naderi@hotmail.com>
2026-04-30 13:44:07 +0330

    Fixed S2 problem with WPA3 SAE

--- lib/app/devices/wifi.cpp
+++ lib/app/devices/wifi.cpp
@@ -56,10 +56,10 @@ void wifi_init_softap(void) {
               .channel = CONFIG_TESLASYNTH_WIFI_CHANNEL,
 #ifdef CONFIG_ESP_WIFI_SOFTAP_SAE_SUPPORT
               .authmode = WIFI_AUTH_WPA3_PSK,
-              .max_connection = 1,
 #else /* CONFIG_ESP_WIFI_SOFTAP_SAE_SUPPORT */
               .authmode = WIFI_AUTH_WPA2_PSK,
 #endif
+              .max_connection = 1,
               .pmf_cfg =
                   {
                       .required = true,
@@ -71,7 +71,7 @@ void wifi_init_softap(void) {
                       .protected_keep_alive = 1,
                   },
 #endif
-              .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,
+              .sae_pwe_h2e = WPA3_SAE_PWE_HASH_TO_ELEMENT,
           },
   };
   if (strlen(CONFIG_TESLASYNTH_WIFI_PASSWORD) == 0) {
