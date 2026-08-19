d488c6d9d4fcfa1afeb2017a18af93bd0a421650
Hossein Naderi <hossein-naderi@hotmail.com>
2026-08-19 11:33:22 +0330

    Updated firmware dashboard about modal

--- web/apps/dashboard/src/About.jsx
+++ web/apps/dashboard/src/About.jsx
@@ -9,10 +9,22 @@ import { Modal } from './components/modal';
 export function About({ open, onClose }) {
     return (
         <Modal title="About" open={open} onClose={onClose}>
-            <Logo size={100} />
+            <p>
+                {' '}
+                <Logo size={100} />
+            </p>
+            <p>
+                MIDI synthesizer that plays music through any interrupted
+                device, including Tesla coils, Flyback transformers, High power
+                lasers, ...
+            </p>
             <ul>
                 <li>
                     Author: <a href="https://hnaderi.dev">Hossein Naderi</a>
+                </li>
+                <li>
+                    Source code:{' '}
+                    <a href="https://github.com/hnaderi/teslasynth">github</a>
                 </li>
                 <li>
                     Contact:{' '}
